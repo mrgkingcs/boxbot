150259ff2ef93795b7cb0b6d510d3b78f578bb9f
GRG <@JammerGRG>
2021-06-12 19:28:07 +0100

    Basic Anims!
    Four basic animation actions: Forward, Backward, Right and Left.
    Changed to fbx export from Blender as glb export was dropping anims.

--- BoxBotProj/test.gd
+++ BoxBotProj/test.gd
@@ -9,11 +9,17 @@ var botMeshAnimationPlayer = null
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	botMeshAnimationPlayer = get_node(("boxbotMesh_anim/AnimationPlayer"))
-	#botMeshAnimationPlayer.play("Action")
+	print(botMeshAnimationPlayer.get_animation_list())
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	#if not botMeshAnimationPlayer.playback_active:
-		if Input.is_key_pressed(KEY_SPACE):
-			botMeshAnimationPlayer.play("Forward")
+		if Input.is_key_pressed(KEY_UP):
+			botMeshAnimationPlayer.play("Armature|Forward")
+		elif Input.is_key_pressed(KEY_DOWN):
+			botMeshAnimationPlayer.play("Armature|Backward")
+		elif Input.is_key_pressed(KEY_LEFT):
+			botMeshAnimationPlayer.play("Armature|Left")
+		elif Input.is_key_pressed(KEY_RIGHT):
+			botMeshAnimationPlayer.play("Armature|Right")
 			
